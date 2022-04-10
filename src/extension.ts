a644602ba48bc8a24313434d79edf4237b0cdd7b
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-04-10 13:59:58 -0400

    feat: add more trigger characters

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
-import { commands, type DocumentSelector, type ExtensionContext, languages } from "vscode";
+import { commands, languages, type DocumentSelector, type ExtensionContext } from "vscode";
 import * as cmds from "./commands";
 import { provideCompletionItems } from "./providers";
+
+const TRIGGER_CHARACTERS = ["/", "*", ".", "(", "~", "!", "#", "$", "@"];
 
 export function activate(context: ExtensionContext): void {
 	const selector: DocumentSelector = { language: "ignore", scheme: "file" };
 
 	context.subscriptions.push(
-		languages.registerCompletionItemProvider(selector, { provideCompletionItems }, ..."/.(*"),
+		languages.registerCompletionItemProvider(selector, { provideCompletionItems }, ...TRIGGER_CHARACTERS),
 		commands.registerCommand("ignore.newFile", cmds.newFile),
 		commands.registerCommand("ignore.ignoreFile", cmds.ignoreFile)
 	);
