99cdb0038a5f32e75a1270ef7b817af7ff32602b
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-07-16 18:57:22 -0400

    feat: add link provider

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,10 +1,12 @@
-import { languages, type DocumentSelector } from "vscode";
+import { languages } from "vscode";
 
 import provideCompletionItems from "./completions";
+import provideDocumentLinks from "./links";
 
-const selector: DocumentSelector = { language: "ignore", scheme: "file" };
+const selector = { language: "ignore", scheme: "file" };
 const triggers = ["/", "*", ".", "(", "~", "!", "#", "$", "@"];
 
 export default [
 	languages.registerCompletionItemProvider(selector, { provideCompletionItems }, ...triggers),
+	languages.registerDocumentLinkProvider(selector, { provideDocumentLinks }),
 ];
---
+++ src/providers/links.ts
@@ -0,0 +1,43 @@
+import path from "path/posix";
+import { Position, Range, Uri, workspace, type DocumentLink, type TextDocument } from "vscode";
+
+export default async function (doc: TextDocument): Promise<DocumentLink[]> {
+	const links: DocumentLink[] = [];
+
+	const lines = doc
+		.getText()
+		.split("\n")
+		.map((line) => line.trim());
+
+	const root = workspace.getWorkspaceFolder(doc.uri)?.uri.fsPath ?? "";
+
+	let lineNum = -1;
+
+	for (const line of lines) {
+		lineNum++;
+		if (line === "" || line.startsWith("#")) continue;
+
+		const normalized = path.normalize(line);
+		const parent = path.isAbsolute(normalized) ? root : path.dirname(doc.fileName);
+
+		const target = Uri.file(path.join(parent, normalized));
+
+		try {
+			await workspace.fs.readFile(target);
+
+			const nwsChar = doc.lineAt(lineNum).firstNonWhitespaceCharacterIndex;
+
+			const start = new Position(lineNum, nwsChar);
+			const end = new Position(lineNum, nwsChar + normalized.length);
+
+			links.push({
+				range: new Range(start, end),
+				target,
+			});
+		} catch {
+			return [];
+		}
+	}
+
+	return links;
+}
