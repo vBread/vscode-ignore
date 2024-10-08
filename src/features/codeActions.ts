77af06ec5bc09c24a82e9d79b9e19c5d46bc6032
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-10-07 23:39:55 -0400

    feat: add code actions

---
+++ src/features/codeActions.ts
@@ -0,0 +1,30 @@
+import { type CodeAction, CodeActionKind, type CodeActionProvider, WorkspaceEdit } from "vscode";
+
+export const provideCodeActions: CodeActionProvider["provideCodeActions"] = async (
+	document,
+	_range,
+	context,
+	_token,
+) => {
+	const actions: CodeAction[] = [];
+
+	for (const diagnostic of context.diagnostics) {
+		// We don't need to check the code since each diagnostic
+		// can be solved by removing the entry
+
+		const edit = new WorkspaceEdit();
+		edit.delete(document.uri, diagnostic.range);
+
+		const [adjective] = (diagnostic.code as string).split("-");
+
+		actions.push({
+			kind: CodeActionKind.QuickFix,
+			title: `Remove ${adjective} entry`,
+			diagnostics: [diagnostic],
+			isPreferred: true,
+			edit,
+		});
+	}
+
+	return actions;
+};
--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,5 @@
 import { languages } from "vscode";
+import { provideCodeActions } from "./codeActions";
 import { provideCompletionItems } from "./completions";
 import { provideDocumentLinks } from "./links";
 
@@ -8,6 +9,7 @@ const selector = { language: "ignore", scheme: "file" };
 const triggers = ["/", "*", ".", "(", "~", "!", "#", "$", "@"];
 
 export const providers = [
+	languages.registerCodeActionsProvider(selector, { provideCodeActions }),
 	languages.registerCompletionItemProvider(selector, { provideCompletionItems }, ...triggers),
 	languages.registerDocumentLinkProvider(selector, { provideDocumentLinks }),
 ];
