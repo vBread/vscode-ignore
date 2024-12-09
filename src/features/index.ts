a8003a96d89e17df57bdf7247aa4aa6f050595d6
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-12-08 19:48:02 -0500

    feat: code lenses

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,5 +1,6 @@
 import { languages } from "vscode";
 import { provideCodeActions } from "./codeActions";
+import { provideCodeLenses } from "./codeLens";
 import { provideCompletionItems } from "./completions";
 import { provideDocumentLinks } from "./links";
 
@@ -10,6 +11,7 @@ const triggers = ["/", "*", ".", "(", "~", "!", "#", "$", "@"];
 
 export const providers = [
 	languages.registerCodeActionsProvider(selector, { provideCodeActions }),
+	languages.registerCodeLensProvider(selector, { provideCodeLenses }),
 	languages.registerCompletionItemProvider(selector, { provideCompletionItems }, ...triggers),
 	languages.registerDocumentLinkProvider(selector, { provideDocumentLinks }),
 ];
